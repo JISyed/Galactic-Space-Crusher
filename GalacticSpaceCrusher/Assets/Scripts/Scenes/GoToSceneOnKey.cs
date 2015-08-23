08a48fd1d10371ee1f9e9ca7443f1c33d5e0033f
Jibran Ibrahim Syed <jisyed14@gmail.com>
2015-08-23 13:48:39 -0500

    Adding a script to go to the specified scene with a specified key

---
+++ GalacticSpaceCrusher/Assets/Scripts/Scenes/GoToSceneOnKey.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GoToSceneOnKey : MonoBehaviour 
+{
+	public KeyCode triggerKey = KeyCode.Return;
+	public string nextScene;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(this.triggerKey))
+		{
+			Application.LoadLevel(this.nextScene);
+		}
+	}
+}
