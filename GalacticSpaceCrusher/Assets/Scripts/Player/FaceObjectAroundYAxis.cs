fb52f1be85f837ddf6e0b3a620bac7c93324392e
Jibran Ibrahim Syed <jisyed14@gmail.com>
2015-08-24 15:02:17 -0500

    Turrets can now be aimed with the mouse!

---
+++ GalacticSpaceCrusher/Assets/Scripts/Player/FaceObjectAroundYAxis.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceObjectAroundYAxis : MonoBehaviour 
+{
+	[SerializeField] private Transform target;
+
+	private Transform theTransform;
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.theTransform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		var lookPos = target.position - this.theTransform.position;
+		lookPos.y = 0;
+		var finalRotation = Quaternion.LookRotation(lookPos);
+		this.theTransform.rotation = finalRotation;
+	}
+}
