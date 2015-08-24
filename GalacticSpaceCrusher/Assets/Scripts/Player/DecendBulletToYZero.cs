5caec81781cc1e1fa64492408f35a07f0faf2a4e
Jibran Ibrahim Syed <jisyed14@gmail.com>
2015-08-24 17:23:00 -0500

    Added a script to make the bullets even with the XZ plane (at y = 0)

---
+++ GalacticSpaceCrusher/Assets/Scripts/Player/DecendBulletToYZero.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class DecendBulletToYZero : MonoBehaviour 
+{
+	[SerializeField] private float decentionSpeed;
+
+	private Transform theTransform;
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.theTransform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(this.theTransform.position.y <= 0.0f)
+		{
+			// Disable the script
+			this.enabled = false;
+		}
+		else
+		{
+			Vector3 newPosition = this.theTransform.position;
+			newPosition += (Vector3.down * this.decentionSpeed) * Time.deltaTime;
+			this.theTransform.position = newPosition;
+		}
+	}
+}
