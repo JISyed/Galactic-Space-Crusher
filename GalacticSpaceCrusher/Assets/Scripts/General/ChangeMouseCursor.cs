bbf7f73064f29bae9823ef514b9f48c24308f650
Jibran Ibrahim Syed <jisyed14@gmail.com>
2015-08-24 13:55:27 -0500

    Now objects can follow mouse on the XZ plane

---
+++ GalacticSpaceCrusher/Assets/Scripts/General/ChangeMouseCursor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ChangeMouseCursor : MonoBehaviour 
+{
+	public Texture2D mousePointer;
+	public Vector2 hotspot;
+	public CursorMode cursorMode;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Cursor.SetCursor(mousePointer, hotspot, cursorMode);
+	}
+
+	void OnDestroy()
+	{
+		Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+	}
+}
---
+++ GalacticSpaceCrusher/Assets/Scripts/Player/FollowMouseOnXAndZPlane.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+// Thanks Byte56 
+// http://gamedev.stackexchange.com/questions/75649/how-do-i-get-mouse-x-y-of-the-world-plane-in-unity
+
+public class FollowMouseOnXAndZPlane : MonoBehaviour
+{
+	private static Plane XZPlane = new Plane(Vector3.up, Vector3.zero);
+
+	private Transform theTransform;
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.theTransform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.theTransform.position = FollowMouseOnXAndZPlane.GetMousePositionOnXZPlane();
+	}
+	
+	public static Vector3 GetMousePositionOnXZPlane() 
+	{
+		float distance;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(XZPlane.Raycast (ray, out distance)) 
+		{
+			Vector3 hitPoint = ray.GetPoint(distance);
+			//Just double check to ensure the y position is exactly zero
+			hitPoint.y = 0;
+			return hitPoint;
+		}
+		
+		return Vector3.zero;
+	}
+}
